2f8bda8e1e50660bc609b25e9bd4947c04aa5032
Andrey <samoilenkoand@gmail.com>
2023-01-27 21:09:08 +0300

    use toolkit createSlice

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
-import { createAction } from "@reduxjs/toolkit";
+import {heroesFetching, heroesFetched, heroesFetchingError} from '../components/heroesList/heroesSlice';
+import {filtersFetching, filtersFetched, filtersFetchingError, activeFilterChanged} from '../components/heroesFilters/filtersSlice';
 
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
@@ -14,21 +15,3 @@ export const fetchFilters = (request) => (dispatch) => {
         .then(data => dispatch(filtersFetched(data)))
         .catch(() => dispatch(filtersFetchingError()))
 }
-
-export const heroesFetching = createAction('HEROES_FETCHING');
-
-export const heroesFetched = createAction('HEROES_FETCHED');
-
-export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR');
-
-export const filtersFetching = createAction('FILTERS_FETCHING');
-
-export const filtersFetched = createAction('FILTERS_FETCHED');
-
-export const filtersFetchingError = createAction('FILTERS_FETCHING_ERROR');
-
-export const activeFilterChanged = createAction('ACTIVE_FILTER_CHANGED');
-
-export const heroDeleted = createAction('HERO_DELETED');
-
-export const heroCreated = createAction('HERO_CREATED');
