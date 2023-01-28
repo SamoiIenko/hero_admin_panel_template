4ba56de2d8a5ba28896c8fbe8ad66e6c8a7f2dc5
Andrey <samoilenkoand@gmail.com>
2023-01-28 19:26:24 +0300

    use toolkit createAsyncThunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,8 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilters} from '../../actions';
-import { activeFilterChanged } from './filtersSlice';
+import { activeFilterChanged, fetchFilters } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 // Задача для этого компонента:
@@ -21,7 +20,7 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchFilters(request));
+        dispatch(fetchFilters());
 
         // eslint-disable-next-line
     }, []);
--- src/components/heroesFilters/filtersSlice.js
+++ src/components/heroesFilters/filtersSlice.js
@@ -1,10 +1,19 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import {useHttp} from '../../hooks/http.hook';
 
 const initialState = {
     filters: [],
     filtersLoadingStatus: 'idle',
     activeFilter: 'all'
 }
+
+export const fetchFilters = createAsyncThunk(
+    'filters/fetchFilters',
+    () => {
+        const {request} = useHttp();
+        return request("http://localhost:3001/filters");
+    }
+);
 
 const filtersSlice = createSlice({
     name: 'filters',
@@ -17,6 +26,16 @@ const filtersSlice = createSlice({
         },
         filtersFetchingError: state => {state.filtersLoadingStatus = 'error'},
         activeFilterChanged: (state, action) => {state.activeFilter = action.payload}
+    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchFilters.pending, state => {state.filtersLoadingStatus = 'loading'})
+            .addCase(fetchFilters.fulfilled, (state, action) => {
+                state.filters = action.payload;
+                state.filtersLoadingStatus = 'idle';
+            })
+            .addCase(fetchFilters.rejected, state => {state.filtersLoadingStatus = 'error'})
+            .addDefaultCase(() => {})
     }
 });
 
